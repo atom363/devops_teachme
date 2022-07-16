dd371c24db895df4347ca4c71ca5d8eff18d0015
Boris Koltun <atom363@gmail.com>
2022-07-16 15:31:27 +0300

    Added branch and lesson 12 files

---
+++ lesson12/script.sh
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red193\green101\blue28;\red0\green0\blue0;\red180\green36\blue25;
+\red200\green20\blue201;}
+{\*\expandedcolortbl;;\cssrgb\c80555\c47366\c13837;\csgray\c0;\cssrgb\c76411\c21697\c12527;
+\cssrgb\c83397\c23074\c82666;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \CocoaLigature0 touch\cf3  file\
+\cf2 echo\cf4  \cf2 "\cf4 \\"\cf5 $(date)\cf4 \\"\cf2 "\cf4  \cf2 >\cf3  file\
+\cf2 echo\cf4  \cf2 "\cf4 '\cf5 $(date)\cf4 '\cf2 "\cf4  \cf2 >>\cf3  file\
+\cf2 echo\cf4  \cf2 "\cf4 \\\cf5 `$(date)\\`\cf2 "\cf4  >> file\cf3 \
+\
+\cf4 a=\cf2 "\cf5 Hello World\cf2 !"\cf3 \
+\cf4 b=\cf5 $(hostname)\cf3 \
+\cf4 c=\cf5 $( \cf2 expr\cf5  \cf4 1\cf5  + \cf4 1\cf5 )\cf3 \
+\
+\cf4 echo -e \cf2 "\cf5 $a\\n$b\\n$c\cf2 "}
