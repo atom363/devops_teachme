04a4cc3a35084d2d15c03440e3d26e4c33a00f55
Boris Koltun <atom363@gmail.com>
2022-08-28 18:36:51 +0300

    Added files for lesson20

---
+++ lesson20/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysqldump -u root -pmynewpassword devops_mysql1 > /opt/mysql_backup/base1_backup_"$(date +%Y-%m-%d-%H)".sql
+mysqldump -u root -pmynewpassword devops_mysql2 > /opt/mysql_backup/base2_backup_"$(date +%Y-%m-%d-%H)".sql
+
+pg_dump postgresql://psql1:psql1@localhost:5432/devops_postgres1 > /opt/postgresql_backup/base1_backup_"$(date +%Y-%m-%d-%H)".sql
+pg_dump postgresql://psql2:psql2@localhost:5432/devops_postgres2 > /opt/postgresql_backup/base2_backup_"$(date +%Y-%m-%d-%H)".sql
+
+crontab -l | grep "0 * * * * ~/backup.sh" || (crontab -l 2>/dev/null; echo "0 * * * * ~/backup.sh") | crontab -
